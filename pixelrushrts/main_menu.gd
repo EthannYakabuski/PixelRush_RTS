ad58cb9269071e7d6e257a217ef70307704af47a
Ethann Yakabuski <ethann.yakabuski@carleton.ca>
2025-02-02 21:58:45 -0500

    add rewarded ad button and view and reward callback

--- pixelrushrts/main_menu.gd
+++ pixelrushrts/main_menu.gd
@@ -1,5 +1,8 @@
 extends Node2D
 var _ad_view : AdView
+var _rewarded_ad : RewardedAd
+var _full_screen_content_callback : FullScreenContentCallback
+var on_user_earned_reward_listener := OnUserEarnedRewardListener.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -38,7 +41,7 @@ func _create_ad_view() -> void:
 	
 	var unit_id = "ca-app-pub-3940256099942544/6300978111"
 
-	_ad_view = AdView.new(unit_id, AdSize.BANNER, AdPosition.Values.BOTTOM_RIGHT)
+	_ad_view = AdView.new(unit_id, AdSize.BANNER, AdPosition.Values.TOP_RIGHT)
 	var ad_request = AdRequest.new()
 	_ad_view.load_ad(ad_request)
 	_ad_view.show()
@@ -47,3 +50,32 @@ func destroy_ad_view():
 	if _ad_view:  
 		_ad_view.destroy()
 		_ad_view = null
+
+
+func _on_ad_button_pressed() -> void:
+	if _rewarded_ad: 
+		_rewarded_ad.destroy()
+		_rewarded_ad = null
+		
+	var unit_id = "ca-app-pub-3940256099942544/5224354917"
+	
+	var rewarded_ad_load_callback := RewardedAdLoadCallback.new()
+	
+	rewarded_ad_load_callback.on_ad_failed_to_load = func(adError: LoadAdError) -> void: 
+		print(adError.message)
+	
+	rewarded_ad_load_callback.on_ad_loaded = func(rewarded_ad: RewardedAd) -> void: 
+		print("rewarded ad loaded")
+		_rewarded_ad = rewarded_ad
+		_rewarded_ad.full_screen_content_callback = _full_screen_content_callback
+		_rewarded_ad.show(on_user_earned_reward_listener)
+		
+	RewardedAdLoader.new().load(unit_id, AdRequest.new(), rewarded_ad_load_callback)
+		
+		
+func on_user_earned_reward(rewarded_item : RewardedItem):
+	print("on_user_earned_reward, rewarded_item: rewarded", rewarded_item.amount, rewarded_item.type)
+	#once we are using an actual unit-id from admob, the rewarded_item.amount and rewarded_item.type values are set in the admob console
+	#for our case, we are rewarding 25 coins to the player and must save it to the user data
+	
+	#TODO - reward player with coins
